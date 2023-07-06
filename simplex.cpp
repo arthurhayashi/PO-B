f17b769ddb7682f471e4d6b82f60757686c7809b
arthurhayashi <arthuryukiohayashi@gmail.com>
2023-07-06 17:55:00 -0300

    simplex inicial

--- simplex.cpp
+++ simplex.cpp
@@ -17,41 +17,120 @@ void imprimirMatriz(const vector<vector<double>>& matriz) {
     }
 }
 
+// Função para permutar linhas da matriz
+void permutarLinhas(vector<vector<double>>& matriz, int i, int j) {
+    int colunas = matriz[0].size();
+
+    for (int k = 0; k < colunas; k++) {
+        double temp = matriz[i][k];
+        matriz[i][k] = matriz[j][k];
+        matriz[j][k] = temp;
+    }
+}
+
+// Função para calcular a matriz inversa
+vector<vector<double>> calcularMatrizInversa(vector<vector<double>>& matriz) {
+    int n = matriz.size();
+
+    // Criar uma matriz expandida com a matriz original e uma matriz identidade
+    vector<vector<double>> matrizExpandida(n, vector<double>(2 * n, 0.0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrizExpandida[i][j] = matriz[i][j];
+        }
+        matrizExpandida[i][i + n] = 1.0;
+    }
+
+    // Aplicar a eliminação de Gauss
+    for (int i = 0; i < n; i++) {
+        if (matrizExpandida[i][i] == 0.0) {
+            // Se o pivô é zero, permuta a linha com uma linha não nula abaixo
+            bool encontrouLinhaNaoNula = false;
+            for (int j = i + 1; j < n; j++) {
+                if (matrizExpandida[j][i] != 0.0) {
+                    permutarLinhas(matrizExpandida, i, j);
+                    encontrouLinhaNaoNula = true;
+                    break;
+                }
+            }
+            if (!encontrouLinhaNaoNula) {
+                cout << "A matriz não é invertível." << endl;
+                return matrizExpandida;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                double ratio = matrizExpandida[j][i] / matrizExpandida[i][i];
+
+                for (int k = 0; k < 2 * n; k++) {
+                    matrizExpandida[j][k] -= ratio * matrizExpandida[i][k];
+                }
+            }
+        }
+    }
+
+    // Normalizar as linhas da matriz expandida
+    for (int i = 0; i < n; i++) {
+        double divisor = matrizExpandida[i][i];
+        for (int j = 0; j < 2 * n; j++) {
+            matrizExpandida[i][j] /= divisor;
+        }
+    }
+
+    // Extrair a matriz inversa da matriz expandida
+    vector<vector<double>> matrizInversa(n, vector<double>(n, 0.0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            matrizInversa[i][j] = matrizExpandida[i][j + n];
+        }
+    }
+
+    return matrizInversa;
+}
+void multiplicaMatriz(vector<vector<double>>& b,vector<vector<double>>& B) {
+	int linhas1 = b.size();
+    int colunas1 = b.size();
+	int linhas2 = B.size();
+    int colunas2 = B.size();
+	
+	vector<vector<double>>product ={{0, 0, 0}}; 
+
+    for (int row = 0; row < linhas1; row++) {
+        for (int col = 0; col < colunas2; col++) {
+            // Multiply the row of A by the column of B to get the row, column of product.
+            for (int inner = 0; inner < linhas2; inner++) {
+                product[row][col] += b[row][inner] * B[inner][col];
+				// cout << b[row][inner] << " " << B[inner][col] << endl;
+            }
+            std::cout << product[row][col] << "  ";
+        }
+        std::cout << "\n";
+    }
+}
+
+
 int main() {
-    int l, c, maxmin;
-    cout << "Digite 0 se o problema for de maximizar e 1 se for para minimizar: ";
-    cin >> maxmin;
+	// N = aN1 aN2 = a3 a4
+    vector<vector<double>> N = {{1,0},
+								{0,1},
+								{0,0}};
+	//B = aB1 aB2 aB3 = a1 a2 a5
+	vector<vector<double>> B = {{1,1,0},
+								{1,0,1},
+								{0,1,1}};
+	imprimirMatriz(B);
+	vector<vector<double>> bT = {{-2,-1, 0}};
+	// {-2,-1,0} * {1,1,0} 
+	// 			   {1,0,1} Linha por Coluna.
+	// 			   {0,1,1}
+	vector<vector<double>> matrizInversa = calcularMatrizInversa(B);
+	imprimirMatriz(bT);	
+	imprimirMatriz(matrizInversa);	
+	multiplicaMatriz(bT,matrizInversa);
+	vector<vector<double>> A;
 
-    cout << "Digite a quantidade de linhas e colunas da matriz considerando a seguinte base: ";
-    cout << "col:  x   y   z   w    Result"  << endl; 
-    cin >> l >> c;
-
-    vector<vector<double>> matriz(l, vector<double>(c, 0.0));
-
-    cout << "Digite os elementos da matriz como o exemplo a seguir: " << endl;
-    if(maxmin == 0){
-        cout << "max: -x + 2y -z -3w"  << endl; 
-    }else{
-        cout << "min: -x + 2y -z -3w"  << endl; 
-    }
-    cout << "col:  x   y   z   w    Result"  << endl; 
-    // cout << " x -y   0  4w  >=  50"  << endl;
-    // cout << "-x  0   z   w   =  50"   << endl;
-    // cout << " 0  y  -z  -w  <=  50"  << endl;
-    for (int i = 0; i < l; i++) {
-        for (int j = 0; j < c; j++) {
-            cin >> matriz[i][j];
-        }
-    }
-    if(maxmin == 0){
-        for (int j = 0; j < c; j++) {
-            matriz[1][j] *= -1; 
-        }
-    }
-    cout << " x  y   z   w    Result"  << endl; 
-    cout << "Matriz inicial:" << endl;
-
-    imprimirMatriz(matriz);
-
+	vector<vector<double>> X, Y;
+	
     return 0;
 }
